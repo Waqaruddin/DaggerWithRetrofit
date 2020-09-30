115794092588d1f928d70c46df6fc7ff609ba907
Syed Waqar Uddin <49626225+Waqaruddin@users.noreply.github.com>
2020-09-30 12:55:54 -0400

    Added splash screen and background color

---
+++ app/src/main/java/com/example/retrofitdemo/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.retrofitdemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    private val delayedTime: Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        var handler = Handler()
+        handler.postDelayed({
+            checkLogin()
+        }, delayedTime)
+    }
+
+    private fun checkLogin() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
